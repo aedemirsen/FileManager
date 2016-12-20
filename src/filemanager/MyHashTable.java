931bab5986d0eb9488069ed5208d7bb110111c96
aedemirsen <aedemirsen@gmail.com>
2016-12-20 12:41:20 +0300

    19.12.2016-fileManager

--- src/filemanager/MyHashTable.java
+++ src/filemanager/MyHashTable.java
@@ -12,60 +12,56 @@ import java.io.File;
  * @author aedemirsen
  */
 public class MyHashTable {
-     
-    private final Value [] values;
+
+    private final Value[] values;
     private int size;
     private final int TABLE_LENGTH;
     private long sourceSize;
     private long destSize;
-    
-    public MyHashTable(int length){
+
+    public MyHashTable(int length) {
         size = 0;
         sourceSize = 0;
         destSize = 0;
         values = new Value[length];
         this.TABLE_LENGTH = length;
     }
-    
-    public int getSize(){
+
+    public int getSize() {
         return this.size;
     }
-    
-    public void put(Value value){        
+
+    public void put(Value value) {
         if (size == TABLE_LENGTH) {
             throw new MyHashTableException("Table is full!");
         }
-        int hash = (int)value.getSizeInBytes() % TABLE_LENGTH;
+        int hash = (int) value.getSizeInBytes() % TABLE_LENGTH;
         if (values[hash] == null) {
             values[hash] = value;
             destSize += value.getSizeInBytes();
             size++;
-        }
-        else { 
-            if (values[hash].isSameValue(value)) {
-                values[hash].increaseQuantity();
+        } else if (values[hash].isSameValue(value)) {
+            values[hash].increaseQuantity();
+        } else {
+            while (values[hash] != null) {
+                hash++;
+                if (hash == TABLE_LENGTH) {
+                    hash = 0;
+                }
+                if (values[hash] != null && values[hash].isSameValue(value)) {
+                    values[hash].increaseQuantity();
+                    return;
+                }
             }
-            else{
-                while(values[hash] != null){
-                    hash++;                    
-                    if (hash == TABLE_LENGTH) {
-                        hash = 0;
-                    }
-                    if (values[hash] != null && values[hash].isSameValue(value)) {
-                        values[hash].increaseQuantity();
-                        return;
-                    }
-                }               
-                values[hash] = value;
-                size++;
-                destSize += value.getSizeInBytes();
-            }
+            values[hash] = value;
+            size++;
+            destSize += value.getSizeInBytes();
         }
         sourceSize += value.getSizeInBytes();
     }
-    
-    public Value[] getValues(){
-        Value [] v = new Value[size];
+
+    public Value[] getValues() {
+        Value[] v = new Value[size];
         int j = 0;
         for (int i = 0; i < TABLE_LENGTH; i++) {
             if (values[i] != null) {
@@ -75,17 +71,17 @@ public class MyHashTable {
         }
         return v;
     }
-    
-    public long getSourceSize(){        
+
+    public long getSourceSize() {
         return this.sourceSize;
     }
-          
-    public long getDestSize(){        
+
+    public long getDestSize() {
         return this.destSize;
     }
-    
-    public File[] getValueFile(){
-        File [] file = new File[size];
+
+    public File[] getValueFile() {
+        File[] file = new File[size];
         int j = 0;
         for (int i = 0; i < TABLE_LENGTH; i++) {
             if (values[i] != null) {
@@ -95,23 +91,23 @@ public class MyHashTable {
         }
         return file;
     }
-    
-    void print(){
+
+    void print() {
         for (Value value : values) {
-            try{
-            System.out.print(value.getSizeInBytes()+"--");}
-            catch(Exception e){System.out.print("NULL--");}
+            try {
+                System.out.print(value.getSizeInBytes() + "--");
+            } catch (Exception e) {
+                System.out.print("NULL--");
+            }
         }
     }
-    
+
 }
 
-class MyHashTableException extends RuntimeException{
-    
-    public MyHashTableException(String msg){
+class MyHashTableException extends RuntimeException {
+
+    public MyHashTableException(String msg) {
         super(msg);
     }
-    
-    
-}
 
+}
